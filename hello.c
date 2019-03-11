eb36e040f573cfe54929c677cc30bd47e9641de7
Jaewon Chung <jaywonchung@snu.ac.kr>
2019-03-11 15:35:06 +0900

    Added comments on main function

--- hello.c
+++ hello.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
 int main(void) {
+/*
+Contrib: Jaewon Chung 03/11/2019
+We don't expect you to undetstand this.
 
+Core function that prints important information
+Args:
+      None
+      
+Returns:
+      0 if successful
+*/
+	
 
 	printf("hello world\n");
 	printf("hell korea\n");
