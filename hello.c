96b9368566cc716d1ae6b758cc2beba834887cce
Dongmin Shin <dongminshin@MacBook-Pro-5.local>
2019-03-11 14:50:13 +0900

    reset

--- hello.c
+++ hello.c
@@ -4,5 +4,14 @@ int main(void) {
 
 
 	printf("hello world\n");
+	printf("hell korea\n");
 
+	printf("dfdfdfdf");
+	
+	printf("hellllllll\n");
+	
+
+	printf("branch b second commit\n");
+
+	return 0;
 }
